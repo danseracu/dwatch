49b13ba2d0fe8fffb9e0139afe7d4e735a3b2bb7
dseracu <dseracu@valeo-it.com>
2024-08-31 18:13:30 +0300

    Bugfix multi arch build

--- dwatch.monitor/monitor.py
+++ dwatch.monitor/monitor.py
@@ -12,53 +12,57 @@ check_time = os.environ.get('CHECK_TIME', 1)
 
 async def main():
     client = ApiClient(user, password)
-    print('Retrieving device')
+    log('Retrieving device')
     device = await client.p115(device_ip)
 
     while True:
-        print('Retrieving info')
+        log('Retrieving info')
         resp = serverUp()
 
         if resp == False:
-            print('Oh no, server is not responding')
+            log('Oh no, server is not responding')
             await resetDevice(device)
         else:
-            print('Very good, device is on')
+            log('Very good, device is on')
 
-        print('Sleepy times')
+        log('Sleepy times')
 
         await asyncio.sleep(60 * check_time)
 
 async def resetDevice(device):
-    print("Resetting device")
+    log("Resetting device")
 
     info = await device.get_device_info()
 
     if info.device_on == True:
-        print("Device is on, turning off")
+        log("Device is on, turning off")
         await device.off()
 
-        print("Wait 5 secs")
+        log("Wait 5 secs")
         await asyncio.sleep(5)
 
-    print("Device is off, turning on")
+    log("Device is off, turning on")
     await device.on()
 
 
 def serverUp():
-    print('On request')
+    log('On request')
 
     try:
         response = requests.get(url)
         if not response.status_code == 200:
-            print(f'Got error from server: {response.to_dict()}')
+            log(f'Got error from server: {response.to_dict()}')
             return False
-        print('Got resp')
+        log('Got resp')
         return response.text == 'OK'
     except:
-        print("Got exception invoking server")
+        log("Got exception invoking server")
         return False
     
 
+def log(message):
+    print(message, flush=True)
+
 if __name__ == "__main__":
+    log('Heeeey')
     asyncio.run(main())
